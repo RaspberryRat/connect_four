873a211f744fe61b37ef830f3c63642b40a6f59e
G <mail@grahamirvine.com>
2022-07-07 16:17:00 -0600

    working on #place_token tests, having trouble with doubles

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,20 +46,20 @@ describe Board do
       end
     end
   end
-end
-  # describe "#place_token" do
-  #   context 'when a column is selected, insert token' do
-  #     it 'inserts "X" token in column 0, row 5' do
-  #       expect { board.place_token(0) }.to change { board.board[0].last }.to('X')
-  #     end
-  #     let(:column) { double(column: [[nil, 'X', 'X', 'X', 'X', 'X']]) }
-  #     subject(:column_almost_full) { described_class.new(column) }
-  #     it 'inserts "X" token in column 0, row 0 when row 1-5 are full' do
-  #       expect { board.place_token(0) }.to change { column_almost_full.board[0].first }.to('X')
-  #     end
-  #     # let(:almost_full_column) { double(:board, [[nil, 'X', 'X', 'X', 'X', 'X']])}
-  #     # it 'expect all rows to be "X" when token placed and rows 1-5 are full' do
-  #     #   expect(board.board[0].all?('X')).to be(true)
-  #     #   board.place_token(0)
-  #   end
-  # end
+  describe "#place_token" do
+    context 'when a column is selected, insert token' do
+      it 'inserts "X" token in column 0, row 5' do
+        expect { board.place_token(0) }.to change { board.board[0].last }.to('X')
+      end
+      let(:column) { double(column: [[nil, 'X', 'X', 'X', 'X', 'X'], [nil]]) }
+      subject(:one_empty_spot) { described_class.new(column) }
+      it 'inserts "X" token in column 0, row 0 when row 1-5 are full' do
+        expect { one_empty_spot.place_token(0) }.to change { one_empty_spot.board[0][0] }.to('X')
+      end
+      # let(:almost_full_column) { double(:board, [[nil, 'X', 'X', 'X', 'X', 'X']])}
+      # it 'expect all rows to be "X" when token placed and rows 1-5 are full' do
+      #   expect(board.board[0].all?('X')).to be(true)
+      #   board.place_token(0)
+    end
+  end
+end insta
